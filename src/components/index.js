450fc824a1f141f07be8cabd90a4260e95bbca67
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-05-26 23:46:21 +0100

    f

--- src/components/index.js
+++ src/components/index.js
@@ -9,5 +9,5 @@ export { default as Email } from './email';
 export { default as Footer } from './footer';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
-export { default as Jobs } from './sections/projects';
+export { default as Jobs } from './sections/jobs';
 export { default as Contact } from './sections/contact';
