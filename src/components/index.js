31dca52140c25d49ca805ce356d0255518f9a6a0
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-05-27 00:09:49 +0100

    fix: update navigation links and project structure

--- src/components/index.js
+++ src/components/index.js
@@ -10,4 +10,5 @@ export { default as Footer } from './footer';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 export { default as Jobs } from './sections/jobs';
+export { default as Featured } from './sections/projects';
 export { default as Contact } from './sections/contact';
