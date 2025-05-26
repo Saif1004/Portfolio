4ca050d3cbef98ff8ec35efd49650abf0f1041dd
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-05-27 00:12:25 +0100

    nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured as Projects, Contact } from '@components';
+import { Layout, Hero, About, Featured as Projects, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
