a4a599e677058cf7338df5a546fdd9d83de22c01
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2025-05-26 01:41:06 +0100

    f

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: 'src/images/logo.jpg',
       },
     },
     {
